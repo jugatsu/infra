36a4a6d1aeacfcc6da55cb07224939150f672983
Anton Kvashenkin <anton.jugatsu@gmail.com>
2017-09-16 13:33:25 +0300

    Add input variables to app module

--- terraform/modules/app/app.tf
+++ terraform/modules/app/app.tf
@@ -1,20 +1,20 @@
 // Ensure app instance is present
 resource "google_compute_instance" "app" {
-  name         = "reddit-app"
-  machine_type = "g1-small"
-  zone         = "europe-west1-b"
-  tags         = ["reddit-app"]
+  name         = "${var.name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.zone}"
+  tags         = "${var.tags}"
 
   // Boot disk for the instance
   boot_disk {
     initialize_params {
-      image = "${var.app_disk_image}"
+      image = "${var.disk_image}"
     }
   }
 
   // Networks to attach to the instance
   network_interface {
-    network = "default"
+    network = "${var.network}"
 
     access_config = {
       nat_ip = "${google_compute_address.app_ip.address}"
@@ -23,25 +23,20 @@ resource "google_compute_instance" "app" {
 
   // Metadata key/value pairs to make available from within the instance
   metadata {
-    sshKeys = "appuser:${file("${var.public_key_path}")}"
+    sshKeys = "${var.username}:${file("${var.public_key_path}")}"
   }
 }
 
 // Ensure static IP address for instance is present
 resource "google_compute_address" "app_ip" {
-  name = "reddit-app-ip"
+  name = "${var.name}-ip"
 }
 
-// Ensure firewall rules are present
-resource "google_compute_firewall" "firewall_puma" {
-  name    = "allow-puma-default"
-  network = "default"
-
-  allow = {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["reddit-app"]
+// Ensure firewall rule for app is present and configured
+module "app_firewall" {
+  source      = "../vpc/firewall"
+  name        = "${var.firewall_name}"
+  network     = "${var.network}"
+  ports       = "${var.firewall_ports}"
+  target_tags = "${var.tags}"
 }
--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -1,5 +1,32 @@
-variable "app_disk_image" {}
+variable "disk_image" {}
+
+variable "machine_type" {
+  default = "g1-small"
+}
+
+variable "zone" {
+  default = "europe-west1-b"
+}
 
 variable "public_key_path" {
   description = "Path to public key used for ssh access"
+}
+
+variable "tags" {
+  type = "list"
+}
+
+variable "name" {}
+
+variable "network" {
+  description = ""
+  default     = "default"
+}
+
+variable "username" {}
+
+variable "firewall_name" {}
+
+variable "firewall_ports" {
+  type = "list"
 }
