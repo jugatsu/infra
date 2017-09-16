70b8496e30d0c7408a5cc60b319c04e801042cb3
Anton Kvashenkin <anton.jugatsu@gmail.com>
2017-09-16 21:01:36 +0300

    Refactor terraform db module to use firewall and vm modules

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,39 +1,18 @@
 // Ensure db instance is present
-resource "google_compute_instance" "db" {
-  name         = "reddit-db"
-  machine_type = "g1-small"
-  zone         = "europe-west1-b"
-  tags         = ["reddit-db"]
-
-  // Boot disk for the instance
-  boot_disk {
-    initialize_params {
-      image = "${var.db_disk_image}"
-    }
-  }
-
-  // Networks to attach to the instance
-  network_interface {
-    network       = "default"
-    access_config = {}
-  }
-
-  // Metadata key/value pairs to make available from within the instance
-  metadata {
-    sshKeys = "appuser:${file("${var.public_key_path}")}"
-  }
+module "db_instance" {
+  source          = "../gce/vm"
+  name            = "${var.name}"
+  tags            = "${var.tags}"
+  disk_image      = "${var.disk_image}"
+  username        = "${var.username}"
+  public_key_path = "${var.public_key_path}"
 }
 
-// Ensure firewall rule for MongoDB is present
-resource "google_compute_firewall" "firewall_mongo" {
-  name    = "allow-mongo-default"
-  network = "default"
-
-  allow = {
-    protocol = "tcp"
-    ports    = ["27017"]
-  }
-
-  target_tags = ["reddit-app"]
-  source_tags = ["reddit-app"]
+// Ensure firewall rule for db instance is present and configured
+module "db_firewall" {
+  source      = "../vpc/firewall"
+  name        = "${var.firewall_name}"
+  network     = "${var.network}"
+  ports       = "${var.firewall_ports}"
+  source_tags = "${var.source_tags}"
 }
--- terraform/modules/db/variables.tf
+++ terraform/modules/db/variables.tf
@@ -1,5 +1,36 @@
-variable "db_disk_image" {}
+variable "disk_image" {}
+
+variable "firewall_name" {}
+
+variable "firewall_ports" {
+  type = "list"
+}
+
+variable "machine_type" {
+  default = "g1-small"
+}
+
+variable "name" {}
+
+variable "network" {
+  description = ""
+  default     = "default"
+}
 
 variable "public_key_path" {
   description = "Path to public key used for ssh access"
+}
+
+variable "source_tags" {
+  type = "list"
+}
+
+variable "tags" {
+  type = "list"
+}
+
+variable "username" {}
+
+variable "zone" {
+  default = "europe-west1-b"
 }
