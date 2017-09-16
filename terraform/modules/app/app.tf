e0b50c4b8683e3b864c9454bd4178f869b31a908
Anton Kvashenkin <anton.jugatsu@gmail.com>
2017-09-16 19:09:01 +0300

    Use vm module for creating instance in app module

--- terraform/modules/app/app.tf
+++ terraform/modules/app/app.tf
@@ -1,30 +1,16 @@
 // Ensure app instance is present
-resource "google_compute_instance" "app" {
-  name         = "${var.name}"
-  machine_type = "${var.machine_type}"
-  zone         = "${var.zone}"
-  tags         = "${var.tags}"
+module "app_instance" {
+  source     = "../gce/vm"
+  name       = "${var.name}"
+  tags       = "${var.tags}"
+  disk_image = "${var.disk_image}"
 
-  // Boot disk for the instance
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
+  access_config = {
+    nat_ip = "${google_compute_address.app_ip.address}"
   }
 
-  // Networks to attach to the instance
-  network_interface {
-    network = "${var.network}"
-
-    access_config = {
-      nat_ip = "${google_compute_address.app_ip.address}"
-    }
-  }
-
-  // Metadata key/value pairs to make available from within the instance
-  metadata {
-    sshKeys = "${var.username}:${file("${var.public_key_path}")}"
-  }
+  username        = "${var.username}"
+  public_key_path = "${var.public_key_path}"
 }
 
 // Ensure static IP address for instance is present
