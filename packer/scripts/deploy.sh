c9c4f90096efcf778fefc5ccbabf9dea763a43ba
Anton Kvashenkin <anton.jugatsu@gmail.com>
2017-09-18 23:38:45 +0300

    Remove files and scripts folders from packer folder
    We use ansible for configuration management purpose.

--- packer/scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Perfoming deploy of reddit app (https://github.com/Artemmkin/reddit.git)
-
-set -o errexit
-
-git clone https://github.com/Artemmkin/reddit.git
-cd reddit && bundle install
-puma -d
