6cc43c74b5637d845f4954711cac291db6050aff
Anton Kvashenkin <anton.jugatsu@gmail.com>
2017-09-23 11:31:22 +0300

    Add version variable to GCP terraform provider
    Pin it to default pessimistic version 0.1

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -11,6 +11,11 @@ variable "google_region" {
   description = "The region to operate under"
 }
 
+variable "google_version" {
+  default = "~> 0.1"
+  description = "The version of provider module"
+}
+
 #################################
 # Github Provider variables
 #################################
