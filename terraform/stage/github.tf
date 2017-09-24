2e63d7204386e0ea15010336b183b952278870e1
Anton Kvashenkin <anton.jugatsu@gmail.com>
2017-09-24 21:21:27 +0300

    Add ansible-role-ruby repository to terraform config

--- terraform/stage/github.tf
+++ terraform/stage/github.tf
@@ -4,9 +4,16 @@ provider "github" {
 }
 
 resource "github_repository" "ansible_role_mongodb" {
-  name        = "ansible-role-mongodb"
-  description = "Ansible Role for MongoDB (https://www.mongodb.com)"
-  has_issues = true
+  name               = "ansible-role-mongodb"
+  description        = "Ansible Role for MongoDB (https://www.mongodb.com)"
+  has_issues         = true
   allow_rebase_merge = false
   allow_squash_merge = false
+}
+
+resource "github_repository" "ansible_role_ruby" {
+  name             = "ansible-role-ruby"
+  description      = "Ansible Role for Ruby programming language (https://www.ruby-lang.org/en/)"
+  has_issues       = true
+  license_template = "bsd-2-clause"
 }
